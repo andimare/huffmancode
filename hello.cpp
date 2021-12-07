6f31c471bbb9cd621bbc6c1c917f8e4e5e0f330d
andimare <andrewo.mare72@gmail.com>
2021-12-07 15:51:28 +0300

    Progressed alot, did most on the the heavy lifting on the huffman code All that is left is creating the menu

--- hello.cpp
+++ hello.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 
 int main(){
-    //cout<<"Hello world"<<endl;
-
+	//Lexicography
 	/*string str1 = "f", str2 = "b";
 	char ch ='a';
 	int y = ch;
@@ -19,22 +18,28 @@ int main(){
 	}else{
 		cout<<"Let's assume the opposite for now"<<endl;
 	}
+	*/
 	
-	
-	string str3 = "omare.txt";
+	string str3 = "huffIn.txt";
 	fstream file;
 	
 	cout<<str3<<endl;
 	
-	file.open(str3, ios::in | ios::out | ios::trunc);
+	file.open(str3, ios::in);
 	
 	if(file.is_open()){
 		cout<<"File successisfully opened, and the name passed as a variable"<<endl;
+		string text;
+		
+		getline(file,text);
+		cout<<text<<endl;
+		
+		file.close();
 	}else{
 		cout<<"Boy, ooh, boy something is a miss."<<endl;
 	}
-	*/
 	
+	/*
 	unordered_map <char, int> freq;
 	
 	for(char ch: "nyabwanga"){
@@ -47,10 +52,42 @@ int main(){
 	for(int i =0; i < 26; i++){
 	//	cout<<chr<<" "<<freq[chr]<<endl;
 		chr++;
+	}*/
+	
+	/*
+	//creating the space trimming functionality
+	string text = "nyabwanga";
+	unordered_map<char, int>freq;
+	
+	//counting the number of the specific charater(space)
+	for(char ch: text){
+		freq[ch]++;
 	}
 	
-    int andrew;
-    cin>>andrew;
+	//looping through the string to rip of the unwanted charater(space)
+	for(int i = 0; i < freq['a']; i++){
+		text.erase(text.find('a'),1);
+		cout<<text<<endl;
+	}
+	
+	
+	//Convertng the string to upper
+	for(int i = 0;i < text.length();i++){
+		text[i] -= 32;
+	}
+	
+	//Finding the respective decimal numbers
+	//To get the difference between them
+	char ch1[2] = {'a','A'};
+	for(int i = 0;i < 2;i++){
+		int b = ch1[i];
+		//cout<<b<<endl;
+	}
+	
+	cout<<text<<endl;
+	*/
+
+	return 0;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,101 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include "Tree.h"
+using namespace std;
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+//namespace for trimming and capitalizing
+namespace andis{
+	string trimSpace(string text){
+		//creating the space trimming functionality
+		unordered_map<char, int>freq;
+	
+		//counting the number of the specific charater(space)
+		for(char ch: text){
+			freq[ch]++;
+		}
+	
+		//looping through the string to rip of the unwanted charater(space)
+		for(int i = 0; i < freq[' ']; i++){
+			text.erase(text.find(' '),1);
+		}
+		return text;
+	}
+	
+	string toUpper(string text){
+		//Convertng the string to upper
+		for(int i = 0;i < text.length();i++){
+			int dec = text[i];
+			//Checking if it's already in caps
+			if(dec > 64 && dec < 91)continue;
+			else text[i] -= 32;
+		}
+		
+		return text;
+	}
 }
+
+int main()
+{
+	//Name of the file with the Original message
+	string fileName;
+	string text;
+	
+	cout<<"enter text file name :";cin>>fileName;
+	
+	fstream file;	
+	file.open(fileName, ios::in);
+	
+	if(file.is_open()){
+		//Reading from the file
+		getline(file,text);
+		
+		text = andis::toUpper(andis::trimSpace(text));
+		cout<<text<<"  closing input file"<<endl;
+		buildHuffmanTree(text);
+		
+		file.close();
+	}else{
+		cout<<"Boy, ooh, boy something is a miss."<<endl;
+	}
+	
+	//cout<<andis::toUpper(andis::trimSpace(text))<<endl;
+	
+	return 0;
+}
+/*
+Huffman Codes are :
+
+c 11111
+h 111100
+f 11101
+r 11100
+t 11011
+p 110101
+i 1100
+g 0011
+l 00101
+a 010
+o 000
+d 10011
+H 00100
+. 111101
+s 0110
+m 0111
+e 110100
+  101
+n 1000
+u 10010
+
+Original string was :
+Huffman coding is a data compression algorithm.
+
+Encoded string is :
+00100100101110111101011101010001011111100010011110010000011101110001101010101011001101011011010101111110000111110101111001101000110011011000001000101010001010011000111001100110111111000111111101
+
+Decoded string is: 
+Huffman coding is a data compression algorithm.
+*/
