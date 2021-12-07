df5f84420dc30d45b9082a2b967e4ad47340365e
andimare <andrewo.mare72@gmail.com>
2021-12-07 20:13:22 +0300

    Added more trimming(Non-alpha)

--- main.cpp
+++ main.cpp
@@ -25,12 +25,19 @@ namespace andis{
 		return text;
 	}
 	
-	string toUpper(string text){
+	string toUpperTrim(string text){
 		//Convertng the string to upper
 		for(int i = 0;i < text.length();i++){
 			int dec = text[i];
+			
 			//Checking if it's already in caps
 			if(dec > 64 && dec < 91)continue;
+			//Trimming out Non-alpha
+			else if(!(dec > 96) || !(dec < 223)){
+				text.erase(i,1);
+			i--;
+			}
+			//converssion to uppercase
 			else text[i] -= 32;
 		}
 		
@@ -55,7 +62,7 @@ int main()
 		//text ="the cats down in an";
 		
 		cout<<text<<"  closing input file"<<endl;
-		text = andis::toUpper(andis::trimSpace(text));
+		text = andis::toUpperTrim(andis::trimSpace(text));
 		buildHuffmanTree(text);
 		
 		file.close();
@@ -68,36 +75,3 @@ int main()
 	
 	return 0;
 }
-/*
-Huffman Codes are :
-
-c 11111
-h 111100
-f 11101
-r 11100
-t 11011
-p 110101
-i 1100
-g 0011
-l 00101
-a 010
-o 000
-d 10011
-H 00100
-. 111101
-s 0110
-m 0111
-e 110100
-  101
-n 1000
-u 10010
-
-Original string was :
-Huffman coding is a data compression algorithm.
-
-Encoded string is :
-00100100101110111101011101010001011111100010011110010000011101110001101010101011001101011011010101111110000111110101111001101000110011011000001000101010001010011000111001100110111111000111111101
-
-Decoded string is: 
-Huffman coding is a data compression algorithm.
-*/
